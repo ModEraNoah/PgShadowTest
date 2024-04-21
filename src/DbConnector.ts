7641e8328e9443b0e735a2657808af880256b934
ModEraNoah <klauck02@outlook.com>
2024-04-21 19:54:04 +0200

    PoC: final steps

--- src/DbConnector.ts
+++ src/DbConnector.ts
@@ -3,20 +3,18 @@ import * as fs from "fs"
 dotenv.config({ path: "./database.env" })
 
 import { Client } from "pg";
-import { describe, expect, test } from "./TestFunctionalities";
 
 export const client = new Client()
 
 client.connect()
 
-const prepData: { schema: string, table: string, values: any[][] }[] = [{ schema: "public", table: "mytable", values: [[1, "someTestValue"], [1411, "someOtherValue"]] }, { schema: "schemaaa", table: "other_table", values: [[69, "noice"]] }];
 
-const simpleReq = async (preparation?: { schema: string, table: string, values: any[][] }[]) => {
-	let query = "SELECT * FROM pg_proc WHERE proname = 'add';"
-	let values: any[] = [];
-	let res = await client.query(query, values)
+export async function prepareTest(functionName: string, preparation?: { schema: string, table: string, values: any[][] }[]) {
+	let query = "SELECT * FROM pg_proc WHERE proname = ($1);";
+	let values: string[] = [functionName];
+	let functionDefinitionReturn = await client.query(query, values)
 	// console.log(res.rows[0])
-	let src: string = res.rows[0].prosrc;
+	let src: string = functionDefinitionReturn.rows[0].prosrc;
 
 	let nextSearchStartIndex = 0
 	while (true) {
@@ -41,29 +39,35 @@ const simpleReq = async (preparation?: { schema: string, table: string, values: 
 		console.log("tableName without schema:", tableName)
 		console.log("schema name:", schemaName)
 
-		console.log("================")
+		// create table clone
 		let query = "SELECT * from information_schema.columns WHERE table_schema = ($1) AND table_name = ($2);"
 		let values: any[] = [schemaName, tableName];
-		let res = await client.query(query, values)
-		console.log(res.rows.length)
-		if (res.rows.length) {
+		let tableInformationReturn = await client.query(query, values);
+		if (tableInformationReturn.rows.length) {
 			if (!foundName.includes("(")) {
 				src = src.replace(` ${foundName} `, ` ${schemaName}.test_${tableName} `);
 			} else {
 				src = src.replace(` ${foundName} `, ` ${schemaName}.test_${tableName}(${foundName.split("(")[1]} `);
 			}
-			console.log("table truely is one");
 			let query = `CREATE TABLE ${schemaName}.test_${tableName} as (select * from ${schemaName}.${tableName}) with no data;`
 			let values: any[] = [];
-			let res = await client.query(query, values);
-			console.log("res of table cloning", res);
+			await client.query(query, values);
 		}
-		console.log("================");
 
 	}
-	console.log("--------------------")
 	//-----------
 	//preparation
+	//
+	await prepareInput(preparation);
+
+	//-----------
+
+	const shadowFunctionSourceText = await craftShadowFunction(functionName, functionDefinitionReturn, src)
+
+	fs.writeFileSync(__dirname + "/blubby.txte", shadowFunctionSourceText)
+}
+
+async function prepareInput(preparation?: any) {
 	if (preparation) {
 		for (let i = 0; i < preparation.length; i++) {
 			let query = `INSERT INTO ${preparation[i].schema}.test_${preparation[i].table} VALUES (`
@@ -80,43 +84,31 @@ const simpleReq = async (preparation?: { schema: string, table: string, values: 
 				const values = valuePair
 				console.log("values for preparation:", values)
 				console.log("query:", query);
-				const res = await client.query(query, values);
+				await client.query(query, values);
 			}
 		}
 	}
+}
 
-	//-----------
-
-	let returnType: number | string = res.rows[0].prorettype
-	let inputTypes: number[] | string[] = res.rows[0].proargtypes.match(/\d+/g).map((inString: string) => { return parseInt(inString) })
-	const inputArgs: string[] = res.rows[0].proargnames
+async function craftShadowFunction(functionName: string, functionDefinitionReturn: any, functionSourceText: string) {
+	let returnType: number | string = functionDefinitionReturn.rows[0].prorettype
+	let inputTypes: number[] | string[] = functionDefinitionReturn.rows[0].proargtypes.match(/\d+/g).map((inString: string) => { return parseInt(inString) })
+	const inputArgs: string[] = functionDefinitionReturn.rows[0].proargnames
 
 	const typeSet = Array.from(new Set([returnType, ...inputTypes]))
-	console.log("typeSet:", typeSet)
 
 	const typeMap = new Map<number, string>()
 
-	query = "SELECT typname, oid FROM pg_type where oid IN ($1);"
-	res = await client.query(query, typeSet);
-	for (let i = 0; i < res.rows.length; i++) {
-		console.log(res.rows[i])
-		typeMap.set(res.rows[i].oid, res.rows[i].typname)
+	const query = "SELECT typname, oid FROM pg_type where oid IN ($1);"
+	let allUsedTypesReturn = await client.query(query, typeSet);
+
+	for (let i = 0; i < allUsedTypesReturn.rows.length; i++) {
+		typeMap.set(allUsedTypesReturn.rows[i].oid, allUsedTypesReturn.rows[i].typname)
 	}
 
-	console.log("typeMap:", typeMap)
-
-	console.log("\nVor Umwandlung:\n")
-	console.log("returntype:", returnType)
-	console.log("inputTypes:", inputTypes)
-	console.log("inputArgs:", inputArgs, "\n")
 
 	returnType = typeMap.get(returnType as number)!
-	inputTypes = inputTypes.map((item) => { return typeMap.get(item as number)! })
-
-	console.log("\nNach Umwandlung:\n")
-	console.log("returntype:", returnType)
-	console.log("inputTypes:", inputTypes)
-	console.log("inputArgs:", inputArgs, "\n")
+	inputTypes = inputTypes.map((item) => { return typeMap.get(item as number)! });
 
 	let argsString = ""
 	for (let i = 0; i < inputArgs.length; i++) {
@@ -128,14 +120,13 @@ const simpleReq = async (preparation?: { schema: string, table: string, values: 
 		};
 	}
 
-	const functionName = "add";
 
 	const prefix = `CREATE OR REPLACE FUNCTION test_${functionName} (${argsString})\n RETURNS ${returnType}\n LANGUAGE plpgsql\nAS $$\n`;
 	const postfix = "\n$$;"
 
-	const result = prefix + src + postfix
-
-	fs.writeFileSync(__dirname + "/blubby.txte", result)
+	return prefix + functionSourceText + postfix
 }
 
-// simpleReq(prepData)
+
+// const prepData: { schema: string, table: string, values: any[][] }[] = [{ schema: "public", table: "mytable", values: [[1, "someTestValue"], [1411, "someOtherValue"]] }, { schema: "schemaaa", table: "other_table", values: [[69, "noice"]] }];
+// prepareTest("add", prepData)
